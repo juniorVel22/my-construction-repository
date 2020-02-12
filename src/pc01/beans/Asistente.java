845d6402f48544188ccd4a4ccf349cefc256a3e3
junior <jota.vela88@gmail.com>
2020-02-12 15:35:46 -0500

    cambio name carpeta

---
+++ src/pc01/beans/Asistente.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pc01.beans;
+
+public class Asistente extends Expositor {
+    private int telefono;
+    private String direccion;
+
+    public Asistente() {
+    }
+    
+    
+    public Asistente(int telefono, String direccion, String codigo, String nombre, String apellido, String correo) {
+        super(codigo, nombre, apellido, correo);
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Asistente{" +"Evento="+evento.getTitulo()+", nombre="+nombre+" "+apellido+", telefono=" + telefono + ", direccion=" + direccion + '}'+"\n";
+    }
+    
+   
+}
---
+++ src/pc01/beans/Constantes.java
@@ -0,0 +1,12 @@
+
+package pc01.beans;
+
+
+public class Constantes {
+    
+    public final double platinum=250.50;
+    public final double gold=150.50;
+    public final double vip=95.00;
+    public  double totalPagar=0.0;
+ 
+}
